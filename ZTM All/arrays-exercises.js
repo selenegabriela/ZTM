7dcf550b54bfcd9d9a71931b46532daf9e6969bd
selenegabriela <sel_nn@hotmail.com>
2023-10-12 21:44:39 -0600

    another way subsum arrays

--- ZTM All/arrays-exercises.js
+++ ZTM All/arrays-exercises.js
@@ -32,14 +32,42 @@ const twoSum2 = (arr, target) => {
 
 }
 
+// [4,-5,2,7,-10,7]
+const maxSubArray2Loops = (nums) => { //O(n2)
+    let largestSum = nums[0];
+    for(let i = 0; i < nums.length; i++){
+        let sum = nums[i]
+        for(let j = i+1; j < nums.length; j++){
+            sum+=nums[j];
+            largestSum = Math.max(largestSum, sum)
+        }
+    }
+    return largestSum;
+}
 
-
-const maxSubArray = (nums) => {
+// [4,-1,2,7,-10,7]
+const maxSubArray = (nums) => { // O(n)
     let maxSum = nums[0];
     for(let i = 1; i < nums.length; i++){
-        nums[i] = Math.max(0, nums[i-1]) + nums[i];
+        nums[i] = Math.max(0, nums[i-1]) + nums[i]; // cuando el número anterior es menor a 0, se reinicia la suma
  
         if(nums[i] > maxSum) maxSum = nums[i]
     }
     return maxSum
 };
+
+console.log(maxSubArray([-10,-3,-2]));
+
+const maxSumArray3 = (nums) => {
+    let sum = 0
+    let max = nums[0]
+
+    for(let number of nums){
+        sum+=number
+        max = Math.max(max, sum)
+
+        if(sum < 0) sum=0
+    }
+    return max;
+}
+
