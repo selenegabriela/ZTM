0d89117b1a411dbf9abafed0025ca932422b23df
selenegabriela <sel_nn@hotmail.com>
2024-01-09 22:10:45 -0600

    hashtable keys

--- ZTM All/hash-table.js
+++ ZTM All/hash-table.js
@@ -1,8 +1,10 @@
 class HashTable {
     constructor(size){
       this.data = new Array(size);
+      this.keys = [];
     }
   
+    // O(1)
     _hash(key) {
       let hash = 0;
       for (let i =0; i < key.length; i++){
@@ -10,18 +12,39 @@ class HashTable {
       }
       return hash;
     }
-
+    // O(1)
     set(key,value){
-        this.data[this._hash(key)] = [key, value]
+        if(!this.data[this._hash(key)]){
+          this.data[this._hash(key)] = []
+        }
+        this.data[this._hash(key)].push([key,value])
+        this.keys.push(key);
     }
 
+    // O(1) - with collisions -> O(n)
     get(key){
-        return this.data[this._hash(key)][1]
+      
+      const bucket = this.data[this._hash(key)]
+
+      if(bucket){
+        const found = bucket.find(arr => arr[0]===key)
+        if (found) return found[1]
+      }
+      return null
+    }
+
+    get_keys(){
+      for(let i = 0; i <= this.keys.length; i++){
+        console.log(this.keys[i])
+      }
     }
   }
   
   const myHashTable = new HashTable(50);
   myHashTable.set('grapes', 10000)
-  console.log(myHashTable.get('grapes'))
+  console.log(myHashTable.get('jeje'))
   myHashTable.set('apples', 9)
-  console.log(myHashTable.get('apples'))
+  myHashTable.get_keys()
+  //console.log(myHashTable.get('apples'))
+
+
