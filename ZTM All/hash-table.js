a5c141fa1a1ccc88f853aec8b057f9bc21771326
selenegabriela <sel_nn@hotmail.com>
2024-02-13 21:35:12 -0600

    linked-list: append, prepend

--- ZTM All/hash-table.js
+++ ZTM All/hash-table.js
@@ -40,11 +40,59 @@ class HashTable {
     }
   }
   
-  const myHashTable = new HashTable(50);
-  myHashTable.set('grapes', 10000)
-  console.log(myHashTable.get('jeje'))
-  myHashTable.set('apples', 9)
-  myHashTable.get_keys()
+  // const myHashTable = new HashTable(50);
+  // myHashTable.set('grapes', 10000)
+  // console.log(myHashTable.get('jeje'))
+  // myHashTable.set('apples', 9)
+  // myHashTable.get_keys()
   //console.log(myHashTable.get('apples'))
 
+const repeatedElement = arr => {
 
+  const mySet = new Set()
+  let repeatedNum = 0
+
+  for(let i = 0; i < arr.length; i++){
+    if(mySet.has(arr[i])){
+      return repeatedNum = arr[i];   
+    }
+    else mySet.add(arr[i])
+  }
+
+
+  return repeatedNum !== 0 ? repeatedNum : undefined
+}
+
+const repeatedElement2 = arr => {
+  
+  const map = {}
+  
+  for(let i = 0; i < arr.length; i++){
+    if(map[i]===arr[i]){
+      return arr[i];
+    } else {
+      map[arr[i]] = arr[i];
+    }
+  }
+  
+  return undefined
+}
+
+function firstRecurringCharacter(input) {
+  let diff = Infinity
+  let number = 0
+  for (let i = 0; i < input.length; i++) {
+    for (let j = i + 1; j < input.length; j++) {
+      if(input[i] === input[j]) {
+        if((j-i)<diff){
+          diff = j-i
+          number = input[i]
+        }
+      }
+    }
+  }
+  return number ? number : undefined;
+}
+
+
+console.log(firstRecurringCharacter([2,5,5,2]))
---
+++ ZTM All/linked-list.js
@@ -0,0 +1,37 @@
+class Node {
+    constructor(value){
+        this.value = value,
+        this.next = null
+    }
+}
+
+class LienkedList {
+    constructor(value){
+        this.head = {
+            value: value,
+            next: null
+        }
+        this.tail = this.head;
+        this.length = 1
+    }
+
+    append(value){
+        const newNode = new Node(value);
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++
+    }
+    prepend(value){
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head = newNode
+        this.length++
+    }
+}
+
+const myList = new LienkedList(10);
+myList.append(5)
+myList.append(16)
+myList.prepend(1)
+
+console.log(myList.head);
