229f58a9e795ef062878e8d6e488ca14214b77e5
selenegabriela <selene.amadordiaz@yougov.com>
2024-02-15 16:02:32 -0600

    linked list remove

--- ZTM All/linked-list.js
+++ ZTM All/linked-list.js
@@ -27,11 +27,52 @@ class LienkedList {
         this.head = newNode
         this.length++
     }
+
+    print(){
+        const arr = []
+        let current = this.head;
+
+        while(current){
+            arr.push(current.value)
+            current = current.next;
+        }
+        return arr;
+    }
+
+    findElementToRemove(index){
+        let prev = this.head;
+        let current = this.head.next;
+        let counter = 1;
+        while(index !== counter){
+            prev = current;
+            current = current.next;
+            counter++
+        }
+        prev.next = current.next;
+        return current.value;
+    }
+
+    remove(index){
+        let removedElement = this.head.value;
+        if(index===0) {
+            this.head = this.head.next
+        } else if (index>=this.length){
+            return null;
+        } else {
+            removedElement = this.findElementToRemove(index);
+        }
+        this.length--;
+        return removedElement;
+    }
 }
 
 const myList = new LienkedList(10);
 myList.append(5)
 myList.append(16)
+myList.append(18)
+myList.append(19)
+myList.append(20)
 myList.prepend(1)
+console.log(myList.remove(0))
 
-console.log(myList.head);
+console.log(myList.print());
