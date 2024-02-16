0b8013e11585b3c55351efd1c82e816c254b0291
selenegabriela <sel_nn@hotmail.com>
2024-02-15 18:20:41 -0600

    double linkedlist

--- ZTM All/linked-list.js
+++ ZTM All/linked-list.js
@@ -66,13 +66,114 @@ class LienkedList {
     }
 }
 
-const myList = new LienkedList(10);
-myList.append(5)
-myList.append(16)
-myList.append(18)
-myList.append(19)
-myList.append(20)
-myList.prepend(1)
-console.log(myList.remove(0))
+class DoubleNode {
+    constructor(value){
+        this.value = value;
+        this.prev = null;
+        this.next = null;
+    }
+}
 
-console.log(myList.print());
+class DoubleLinkedList {
+    constructor(value){
+        this.head = {
+            value: value,
+            next: null,
+            prev: null,
+        };
+        this.next = null;
+        this.prev = this.head;
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    print(){
+        const arr = []
+        let current = this.head;
+
+        while(current){
+            arr.push(current.value)
+            current = current.next;
+        }
+        return arr;
+    }
+
+    append(value){
+        const newNode = new DoubleNode(value);
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+    }
+
+    prepend(value){
+        const newNode = new DoubleNode(value);
+        newNode.prev = this.head;
+        this.head = newNode;
+        newNode.next = this.prev;
+        this.length++;
+    }
+
+    traversLinkedList(index){
+        let prev = this.head;
+        let current = this.head.next;
+        let counter = 1;
+
+        while(index !== counter){
+            counter++;
+            prev = current;
+            current = current.next;
+        }
+        return prev;
+    }
+
+    insert(index,value){
+        
+        if(index===0) this.prepend(value);
+        else if(index>=this.length) this.append(value);
+        else {
+            const newNode = new DoubleNode(value);
+            let prev = this.traversLinkedList(index);
+    
+            newNode.next = prev.next;
+            newNode.prev = prev;
+            prev.next = newNode
+            this.length++
+        }
+    }
+
+    remove(index) {
+        let removedElement = this.head.value;
+        if(index===0){
+            this.head = this.head.next;
+        } else {
+            let prev = this.traversLinkedList(index);
+            removedElement = prev.next.value;
+            prev.next = prev.next.next;
+            prev.prev = prev;
+        }
+        this.length--;
+        return removedElement;
+    }
+}
+
+const myDoubleLinkedList = new DoubleLinkedList(30)
+myDoubleLinkedList.append(2)
+myDoubleLinkedList.append(4)
+ myDoubleLinkedList.prepend(6)
+myDoubleLinkedList.insert(2,8)
+console.log(myDoubleLinkedList.remove(4))
+console.log(myDoubleLinkedList)
+console.log(myDoubleLinkedList.print());
+//console.log(myDoubleLinkedList)
+
+// const myList = new LienkedList(10);
+// myList.append(5)
+// myList.append(16)
+// myList.append(18)
+// myList.append(19)
+// myList.append(20)
+// myList.prepend(1)
+// console.log(myList.remove(0))
+
+// console.log(myList.print());
