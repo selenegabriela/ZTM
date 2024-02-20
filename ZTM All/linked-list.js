49872c071ed57e21593f720dab5e5125fae4e7ef
selenegabriela <sel_nn@hotmail.com>
2024-02-20 12:57:07 -0600

    keeping local changes before merging

--- ZTM All/linked-list.js
+++ ZTM All/linked-list.js
@@ -27,11 +27,48 @@ class LienkedList {
         this.head = newNode
         this.length++
     }
+    printList(){
+        const arr = []
+        let current = this.head;
+
+        while(current){
+            arr.push(current.value)
+            current = current.next;
+        }
+        return console.log(arr)
+    }
+    insert(index, value){
+        if(index===0) this.prepend(value)
+        else if(index===this.length) this.append(value)
+        else if(index > this.length || index < 0) return null;
+        else {
+            const newNode = new Node(value)
+            let aux = this.findTheNode(index)
+            let current = aux.next;
+            newNode.next = current;
+            aux.next = newNode;
+            this.length++
+            }
+        }
+    findTheNode(index){
+        let current = this.head.next;
+        let aux = this.head;
+        let counter = 1
+        while(index !== counter){
+                counter++
+                aux = current;
+                current = current.next;
+        }
+        return aux;
+    }
 }
 
 const myList = new LienkedList(10);
 myList.append(5)
 myList.append(16)
+myList.append(17)
+myList.append(18)
 myList.prepend(1)
+console.log(myList.insert(6,111))
 
-console.log(myList.head);
+myList.printList()
