b0548d12ccefd940a5f65e0bc2df2b766f9724f8
selenegabriela <sel_nn@hotmail.com>
2024-03-27 18:30:11 -0600

    queues with push and put methods (stack)

--- ZTM All/queue-stack.js
+++ ZTM All/queue-stack.js
@@ -37,17 +37,52 @@
 //     }
 // }
 
-// const myQueue = new CrazyQueue();
-// console.log(myQueue.peek());
-// myQueue.enqueue('Joy');
-// myQueue.enqueue('Matt');
-// myQueue.enqueue('Pavel');
-// console.log(myQueue.peek());
-// console.log("========");
-// console.log(myQueue.dequeue());
-// console.log(myQueue.dequeue());
-// console.log(myQueue.dequeue());
-// console.log("========");
-// console.log(myQueue.peek());
 
-// console.log(myQueue);
+class QueueStack {
+    constructor(){
+        this.first = []
+        this.last = []
+    }
+
+    enqueue(value){
+        const length = this.first.length;
+
+        for(let i = 0; i < length; i++){
+            this.last.push(this.first.pop())
+        }
+        this.last.push(value)
+    }
+    dequeue(){
+        const length = this.last.length;
+        
+        for(let i = 0; i < length; i++){
+            this.first.push(this.last.pop())
+        }
+        return this.first.pop()
+    }
+    peek(){
+        if(this.first.length){
+            return this.first[this.first.length - 1]
+        }
+
+        return this.last[0]
+    }
+}
+
+const myQueue = new QueueStack();
+console.log(myQueue.peek());
+myQueue.enqueue('Joy');
+myQueue.enqueue('Matt');
+myQueue.enqueue('Pavel');
+console.log(myQueue.peek());
+console.log("========");
+console.log(myQueue.dequeue());
+console.log(myQueue.dequeue());
+console.log(myQueue);
+myQueue.enqueue('Panqué');
+console.log(myQueue.dequeue());
+console.log("========");
+console.log(myQueue.peek())
+console.log(myQueue.dequeue());
+
+console.log(myQueue);
