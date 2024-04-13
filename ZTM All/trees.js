29191ec4d9c46d4aea89c91fa100a1d2e26cf8fb
selenegabriela <sel_nn@hotmail.com>
2024-04-12 22:35:53 -0600

    trees insert and lookup loops

--- ZTM All/trees.js
+++ ZTM All/trees.js
@@ -57,8 +57,70 @@ class BinarySearchTree{
 
 
 }
+// LOOPS =P
+class BinarySearchTree2{
+    constructor(){
+        this.root = null;
+    }
 
-const tree = new BinarySearchTree()
+    insert(value){
+        const newNode = new Node(value)
+        if(!this.root) return this.root = newNode;
+
+        let current = this.root;
+        while(true){
+            if(value > current.value){
+                if(!current.right){
+                    return current.right = newNode;
+                } else {
+                    current = current.right;
+                }
+            } else {
+                if(!current.left){
+                    return current.left = newNode;
+                } else {
+                    current = current.left;
+                }
+            }
+            
+        }
+    }
+
+    lookup(value){
+        if(!this.root) return false;
+        if(this.root.value===value) return true;
+
+        let current = this.root;
+
+        while(true){
+            if(value > current.value){
+                if(!current.right) return false
+                else if(current.right.value===value) return true;
+                else current = current.right
+            } else {
+                if(!current.left) return false
+                else if(current.left.value===value) return true;
+                else current = current.left
+
+            }
+        }
+
+    }
+
+    remove(value){
+        
+    }
+    traverse(node){
+        const tree = {value: node.value}
+        tree.left = !tree.left ? null : this.traverse(node.left)
+        tree.right = !tree.right ? null : this.traverse(node.right)
+        return tree;
+    }
+
+
+}
+
+const tree = new BinarySearchTree2()
 tree.insert(38)
 tree.insert(40)
 tree.insert(20)
